0d2b78199d723a8bcf68a232969519f88530e517
achris-alonzo30 <achris.alonzo30@gmail.com>
2025-01-07 16:16:10 -0400

    fix issue System Default Theme Not Applied in Edge/Chrome Browser

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -2,6 +2,7 @@
 
 import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
 import { Theme } from "@radix-ui/themes";
+import useMediaQuery from '@/hook/useMediaQuery';
 
 type ThemeType = 'inherit' | 'light' | 'dark';
 
@@ -18,17 +19,32 @@ interface ThemeProviderProps {
 
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
     const [theme, setTheme] = useState<ThemeType>('inherit');
+    const prefersDark = useMediaQuery('(prefers-color-scheme: dark)');
 
     useEffect(() => {
         const storedTheme = window.localStorage.getItem('x-theme') as ThemeType;
         if (storedTheme && ['inherit', 'light', 'dark'].includes(storedTheme)) {
             setTheme(storedTheme);
+        } else if (prefersDark) {
+            setTheme('dark');
         }
-    }, []);
+    }, [prefersDark]);
+
+    useEffect(() => {
+        if (theme === 'dark' || (theme === 'inherit' && prefersDark)) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    }, [theme, prefersDark]);
 
     const changeTheme = (newTheme: ThemeType) => {
         setTheme(newTheme);
-        window.localStorage.setItem('x-theme', newTheme);
+        if (newTheme !== 'inherit') {
+            window.localStorage.setItem('x-theme', newTheme);
+        } else {
+            window.localStorage.removeItem('x-theme');
+        }
     };
 
     return (
@@ -38,7 +54,6 @@ export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
     );
 };
 
-// Custom hook to use the ThemeContext
 export const useTheme = (): ThemeContextType => {
     const context = useContext(ThemeContext);
     if (!context) {
